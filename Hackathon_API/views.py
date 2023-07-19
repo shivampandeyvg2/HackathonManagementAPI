d535d4169e02001983f95c2f7e8d4f51843384c5
shivampandeyvg2 <shivampandeyvg@gmail.com>
2023-07-20 03:15:18 +0530

    endpoint for updating hackathon data

--- Hackathon_API/views.py
+++ Hackathon_API/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import  Hackathon
+from .models import Hackathon
 from .serializers import HackathonDataSerializer
 
 
@@ -15,19 +15,18 @@ class hackathon_details(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
-        hackathonList  = Hackathon.objects.all()
-        serializer= HackathonDataSerializer(hackathonList, many=True)
+        hackathonList = Hackathon.objects.all()
+        serializer = HackathonDataSerializer(hackathonList, many=True)
         return Response(serializer.data)
 
-    def put(self, request , pk):
+    def put(self, request, pk):
         try:
             existingHackathonDetails = Hackathon.objects.get(pk=pk)
         except Hackathon.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND , message="No results found for given Hackathon id ")
-        serializer = HackathonDataSerializer(existingHackathonDetails , data =request.data)
+            return Response(status=status.HTTP_404_NOT_FOUND, message="No results found for given Hackathon id ")
+
+        serializer = HackathonDataSerializer(existingHackathonDetails , data=request.data , partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
